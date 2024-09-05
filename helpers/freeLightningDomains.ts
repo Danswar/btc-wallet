6b3fe9b2c4b6b565a6d36aee9827452af7728129
Daniel Padrino <danswarrior1@gmail.com>
2024-09-05 11:08:00 -0300

    [NO-TASK]: remove free domain

--- helpers/freeLightningDomains.ts
+++ helpers/freeLightningDomains.ts
@@ -1,6 +1,6 @@
 export const isFreeDomain = (domain: string) => {
   if (!domain || typeof domain !== 'string') return false;
 
-  const freeDomains = ['lightning.space', 'dev.lightning.space', 'dfx.swiss', 'api.dfx.swiss'];
+  const freeDomains = ['lightning.space', 'dev.lightning.space'];
   return freeDomains.includes(domain.toLowerCase());
 };
